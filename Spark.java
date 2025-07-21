ddd0f36af486ff3e168e1ffbae756844352b29a6
HemantBodade <bodadeh43@gmail.com>
2025-07-21 13:38:53 +0530

    Next done!

--- Spark.java
+++ Spark.java
@@ -2,4 +2,8 @@ class Spark{
     public static void main(String[] args) {
         System.out.println("Hello Git");
     }
+
+    public void Paytm(){
+        System.out.println("Done!");
+    }
 }
